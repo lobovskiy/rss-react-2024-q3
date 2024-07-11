48f80b81f2a702f296b88316fc81645b1922cb60
Artem Lobovskii <lobovskiy@yandex.ru>
2024-07-12 00:46:53 +0300

    refactor: migrate person list to functional component

--- src/components/PersonList.tsx
+++ src/components/PersonList.tsx
@@ -1,32 +1,26 @@
-import { Component } from 'react';
-
 import { IPerson } from '../types.ts';
 
-interface PersonListProps {
+interface Props {
   people: IPerson[];
   progress: boolean;
 }
 
-class PersonList extends Component<PersonListProps> {
-  render() {
-    const { people, progress } = this.props;
-
-    if (progress) {
-      return <div>Loading...</div>;
-    }
-
-    return (
-      <div className="people">
-        {people.map((result, index) => (
-          <div key={index} className="result-item">
-            <h3>Name: {result.name}</h3>
-            <p>Gender: {result.gender}</p>
-            <p>Year of birth: {result.birth_year}</p>
-          </div>
-        ))}
-      </div>
-    );
+const PersonList: React.FC<Props> = ({ people, progress }) => {
+  if (progress) {
+    return <div>Loading...</div>;
   }
-}
+
+  return (
+    <div className="people">
+      {people.map((result, index) => (
+        <div key={index} className="result-item">
+          <h3>Name: {result.name}</h3>
+          <p>Gender: {result.gender}</p>
+          <p>Year of birth: {result.birth_year}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default PersonList;
