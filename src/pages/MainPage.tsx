ebdcc145205b88bdc5e2155af6e4f486e922b1a8
Artem Lobovskii <lobovskiy@yandex.ru>
2024-07-12 01:22:06 +0300

    refactor: migrate main page to functional component, remove options from api service

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 
 import { fetchResults } from '../services/apiService';
 import Search from '../components/Search';
@@ -7,73 +7,66 @@ import { IPerson } from '../types.ts';
 
 import './MainPage.css';
 
-interface MainPageState {
-  searchTerm: string;
-  results: IPerson[];
+interface IPersonList {
+  people: IPerson[];
   progress: boolean;
 }
 
-class MainPage extends Component<NonNullable<unknown>, MainPageState> {
-  constructor(props: NonNullable<unknown>) {
-    super(props);
-    this.state = {
-      searchTerm: localStorage.getItem('searchTerm') ?? '',
-      results: [],
-      progress: false,
-    };
-  }
+const MainPage: React.FC = () => {
+  const initialSearchTerm = localStorage.getItem('searchTerm') ?? '';
+  const [searchTerm, setSearchTerm] = useState<string>(initialSearchTerm);
+  const [personList, setPersonList] = useState<IPersonList>({
+    people: [],
+    progress: false,
+  });
 
-  componentDidMount() {
-    this.loadResults();
-  }
-
-  loadResults = () => {
-    const { searchTerm } = this.state;
-    const onSuccess = () => this.setState({ progress: false });
-    const onError = () => this.setState({ progress: false });
-
-    this.setState({ progress: true }, () => {
-      fetchResults(searchTerm.trim(), { onSuccess, onError })
-        .then((results) => {
-          this.setState({ results });
-        })
-        .catch((error) => {
-          console.error('Error fetching results:', error);
+  const loadResults = useCallback(() => {
+    setPersonList((list) => ({ ...list, progress: true }));
+    fetchResults(searchTerm.trim())
+      .then((results) => {
+        setPersonList({
+          people: results,
+          progress: false,
         });
-    });
-  };
+      })
+      .catch((error) => {
+        console.error('Error fetching results:', error);
+        setPersonList((list) => ({ ...list, progress: true }));
+      });
+  }, [searchTerm]);
 
-  handleSearch = (searchTerm: string) => {
+  useEffect(() => {
+    loadResults();
+  }, [loadResults]);
+
+  function handleSearch(searchTerm: string) {
     localStorage.setItem('searchTerm', searchTerm);
 
-    this.setState({ searchTerm }, this.loadResults);
-  };
-
-  setInvalidState = () => {
-    this.setState({ results: {} as [] });
-  };
-
-  render() {
-    const { searchTerm, results, progress } = this.state;
-
-    return (
-      <div className="main-page">
-        <div className="top-section">
-          <Search searchTerm={searchTerm} onSearch={this.handleSearch} />
-          <button
-            onClick={() => {
-              this.setInvalidState();
-            }}
-          >
-            Throw Error
-          </button>
-        </div>
-        <div className="bottom-section">
-          <PersonList people={results} progress={progress} />
-        </div>
-      </div>
-    );
+    setSearchTerm(searchTerm);
+    loadResults();
   }
-}
+
+  function setInvalidState() {
+    setPersonList({ ...personList, people: {} as [] });
+  }
+
+  return (
+    <div className="main-page">
+      <div className="top-section">
+        <Search searchTerm={searchTerm} onSearch={handleSearch} />
+        <button
+          onClick={() => {
+            setInvalidState();
+          }}
+        >
+          Throw Error
+        </button>
+      </div>
+      <div className="bottom-section">
+        <PersonList people={personList.people} progress={personList.progress} />
+      </div>
+    </div>
+  );
+};
 
 export default MainPage;
--- src/services/apiService.ts
+++ src/services/apiService.ts
@@ -5,35 +5,18 @@ interface IApiResult {
   results: IPerson[];
 }
 
-interface IApiServiceOptions {
-  onSuccess?: () => void;
-  onError?: () => void;
-}
-
-export const fetchResults = async (
-  searchTerm: string,
-  options: IApiServiceOptions
-) => {
+export const fetchResults = async (searchTerm: string) => {
   const API_URl = searchTerm
     ? `https://swapi.dev/api/people/?search=${searchTerm}`
     : 'https://swapi.dev/api/people';
-  const { onSuccess, onError } = options;
 
   const response = await fetch(API_URl);
 
   if (!response.ok) {
-    if (onError) {
-      onError();
-    }
-
     throw new Error('Network response was not ok');
   }
 
   const data = (await response.json()) as IApiResult;
-
-  if (onSuccess) {
-    onSuccess();
-  }
 
   return data.results;
 };
