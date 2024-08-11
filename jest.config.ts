04861644555e5fef6c0915c2153194b5a2720a3b
Artem Lobovskii <lobovskiy@yandex.ru>
2024-08-11 22:43:53 +0300

    feat: migrate app components to working with remix

--- jest.config.ts
+++ jest.config.ts
@@ -20,11 +20,11 @@ const config: Config = {
 
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-      '<rootDir>/src/__mocks__/fileMock.ts',
-    '\\.(css|s[ca]ss|less)$': '<rootDir>/src/__mocks__/styleMock.ts',
-    'swiper/css': '<rootDir>/src/__mocks__/styleMock.ts',
+      '<rootDir>/app/__mocks__/fileMock.ts',
+    '\\.(css|s[ca]ss|less)$': '<rootDir>/app/__mocks__/styleMock.ts',
+    'swiper/css': '<rootDir>/app/__mocks__/styleMock.ts',
   },
-  roots: ['<rootDir>/src'],
+  roots: ['<rootDir>/app'],
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[tj]s?(x)'],
@@ -34,11 +34,11 @@ const config: Config = {
   },
   resetMocks: false,
   setupFiles: ['jest-localstorage-mock'],
-  collectCoverageFrom: ['src/**'],
+  collectCoverageFrom: ['app/**'],
   coveragePathIgnorePatterns: [
     'node_modules',
-    './src/__mocks__',
-    './src/__tests__',
+    './app/__mocks__',
+    './app/__tests__',
   ],
 };
 
