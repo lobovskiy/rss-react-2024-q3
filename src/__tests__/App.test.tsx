c6860dc21fabafb115598057397daf70ebed4a0c
Artem Lobovskii <lobovskiy@yandex.ru>
2024-08-11 17:43:41 +0300

    fix: app test

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -6,20 +6,49 @@ import { useRouter, useSearchParams } from 'next/navigation';
 import { ThemeContext } from '../context/ThemeContext';
 import App from '../App';
 import Card from '../components/Card/Card';
+import * as apiService from '../services/apiService';
+import { peopleMock, personMock } from '../__mocks__/people';
 
 jest.mock('next/navigation', () => ({
   useRouter: jest.fn(),
   useSearchParams: jest.fn(),
 }));
 
+jest.mock('../services/apiService', () => ({
+  __esModule: true,
+  ...jest.requireActual<typeof apiService>('../services/apiService'),
+}));
+
+jest.mock('../components/Card/Card', () => {
+  return function CardMock() {
+    return <div>Card Component</div>;
+  };
+});
+
 describe('App', () => {
   const pushMock = jest.fn();
+  const useGetPeopleQueryMock = jest.spyOn(
+    apiService,
+    'useGetPeopleQuery'
+  ) as jest.Mock;
+  const useGetPeopleByIdQueryMock = jest.spyOn(
+    apiService,
+    'useGetPersonByIdQuery'
+  ) as jest.Mock;
 
   beforeEach(() => {
     jest.clearAllMocks();
     (useRouter as jest.Mock).mockReturnValue({ push: pushMock });
     (useSearchParams as jest.Mock).mockReturnValue({
       get: jest.fn().mockReturnValue(null),
+    });
+    useGetPeopleQueryMock.mockReturnValue({
+      data: { count: 82, results: peopleMock },
+      isLoading: false,
+    });
+    useGetPeopleByIdQueryMock.mockReturnValue({
+      data: personMock,
+      isLoading: false,
     });
   });
 
@@ -36,7 +65,7 @@ describe('App', () => {
   test('should render the Card component with person card component', () => {
     renderComponent(Card);
 
-    expect(screen.getByText('Chosen person')).toBeInTheDocument();
+    expect(screen.getByText('Card Component')).toBeInTheDocument();
   });
 
   test('should apply the correct theme', () => {
