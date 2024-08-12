f8039a6c538cfc91d47152e6d52227fa539f0644
Artem Lobovskii <lobovskiy@yandex.ru>
2024-08-13 02:16:38 +0300

    fix: netlify build

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  distDir: './dist', // Changes the build output directory to `./dist/`.
+  reactStrictMode: true,
 }
 
 export default nextConfig
